c92d392587eae8e1bbc4de92b22fb895bb5bd11b
Maria Rios <kanelapr@gmail.com>
2022-09-29 18:23:18 -0400

    user controller crud

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -27,5 +27,51 @@ users.get('/', async (req, res) => {
     }
 })
 
+//CREATE A USER
+users.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body)
+        res.status(200).json({
+            message: 'Successfully created a new user',
+            data: newUser
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+//UPDATE A USER
+users.put('/:id', async (req, res) => {
+    try {
+        const updatedUsers = await User.update(req.body, {
+            where: {
+                user_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedUsers} user(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+//DELETE A USER
+users.delete('/:id', async (req, res) => {
+    try {
+        const deletedUsers = await User.destroy({
+            where: {
+                user_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedUsers} user(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+
 
 module.exports = users
