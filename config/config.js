e7703b58493d7d04ca25ae3b4537ada6c83652b6
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-10-02 13:45:20 -0400

    fix, again

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
+    "port": 5432,
     "dialect": "postgres",
     "dialectOptions": {
       "ssl": {
